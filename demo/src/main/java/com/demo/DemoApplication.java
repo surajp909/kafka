718235cd9f5730ceeb3de57cc7f5b59477462cf5
surajp909 <89259034+surajp909@users.noreply.github.com>
2022-12-21 11:46:24 +0530

    Add files via upload

---
+++ demo/src/main/java/com/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+import com.demo.kafkaProducer.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	private KafkaProducer kafkaProducer;
+
+	@Override
+	public void run(String... args) throws Exception {
+		kafkaProducer.sendMessage("hii");
+	}
+}
